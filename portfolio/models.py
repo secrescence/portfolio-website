3c8da44008a4942075c40ea13ea362c9fa133f7b
secrescence <edselpisuena@gmail.com>
2025-04-11 20:51:56 +0800

    Update models.py and migration for project image field now switched to CloudinaryField

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -6,7 +7,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="project_images/", blank=True, null=True)
+    image = CloudinaryField("image", blank=True, null=True)
     url = models.URLField(max_length=200)
 
     def __str__(self):
