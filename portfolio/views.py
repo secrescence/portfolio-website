0a6de3986bd1947dd5bcfd128b3f170d98925372
secrescence <edselpisuena@gmail.com>
2025-04-13 00:13:57 +0800

    Added About model, update views and templates to display dynamic content and update requirements.txt

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Project
+from .models import About
+import markdown
 
 # Create your views here.
 
@@ -21,3 +23,15 @@ def projects(request):
     return render(
         request, "portfolio/projects.html", {"projects": projects_list}
     )  # fetch all projects from the database and pass them to the template
+
+
+def about(request):
+    about = About.objects.first()
+    return render(
+        request,
+        "portfolio/about.html",
+        {
+            "what_i_do": markdown.markdown(about.what_i_do),
+            "core_skills": markdown.markdown(about.core_skills),
+        },
+    )
